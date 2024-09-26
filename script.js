bb51d077f1fbac5502dd7f06c7da81c27ad06e43
ahmadhakimitsnaini <ahmadhakimitsnaini@outlook.com>
2024-09-26 07:47:31 +0700

    Calorie Counter Step 75

--- script.js
+++ script.js
@@ -38,6 +38,14 @@ function calculateCalories(e) {
 
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
   const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
+  const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
+  const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+  const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs)
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs)
 
 }
 
